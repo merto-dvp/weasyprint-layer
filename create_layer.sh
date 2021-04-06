0f9d67953108f3e0cdbd6b9d9d6910e4bbf7f80d
Mert <mert@sufle.io>
2021-04-06 11:23:50 +0300

    initial

---
+++ create_layer.sh
@@ -0,0 +1,29 @@
+
+#!/bin/bash
+RUNTIME="python3.8"
+ECHO $RUNTIME
+
+mkdir -p build
+docker run --rm \
+	    -e INSTALL_MS_FONTS="${INSTALL_MS_FONTS}" \
+	    -v `pwd`/fonts:/out \
+	    -t lambci/lambda:build-${RUNTIME} \
+	    bash /out/layer_builder.sh
+mv -f fonts/layer.zip ./build/fonts-layer.zip
+
+docker run --rm \
+	    -v `pwd`/weasyprint:/out \
+	    -t lambci/lambda:build-${RUNTIME} \
+	    bash /out/layer_builder.sh
+	mv -f ./weasyprint/layer.zip ./build/weasyprint-no-fonts-layer.zip
+	cd build && rm -rf ./opt && mkdir opt \
+	    && unzip fonts-layer.zip -d opt \
+	    && unzip weasyprint-no-fonts-layer.zip -d opt \
+	    && cd opt && zip -r9 ../weasyprint-layer-${RUNTIME}.zip .
+pwd
+ls -lah
+mv -f ../weasyprint-layer-${RUNTIME}.zip ../../weasyprint-layer.zip
+
+
+
+
